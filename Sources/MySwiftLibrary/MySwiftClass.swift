4e13900df7e7777d699824988c5b9b3913b529d0
RadonW <radon@126.com>
2021-04-25 16:26:34 +0800

    Initial Commit

---
+++ Sources/MySwiftLibrary/MySwiftClass.swift
@@ -0,0 +1,17 @@
+//
+//  File.swift
+//  
+//
+//  Created by Xudong Wang on 2021/4/23.
+//
+
+import Foundation
+
+public struct MySwiftClass {
+    public let name = "MySwiftClass"
+    
+    public init() {
+        
+    }
+}
+
